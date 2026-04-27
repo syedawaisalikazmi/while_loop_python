22566b720d8ca56af2a089f001fa147afff81140
Syed Awais Ali <awisyedhere.ajk@gmail.com>
2026-04-27 16:34:59 +0500

    Add food entry input loop

---
+++ food_entry.py
@@ -0,0 +1,6 @@
+food=input("enter the food name you like (for quit enter 'q' ):")
+
+while not food=="q":
+    print(f"so you like {food} ")
+    food=input("enter the food name you like (for quit enter 'q' ):")
+print("BYE")
